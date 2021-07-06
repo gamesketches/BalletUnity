3554fd4da8932158ca11f9d6eb3bce48138859c2
gamesketches <sam.von.ehren@gmail.com>
2021-07-06 17:06:55 -0400

    Basic positions and tendus in

--- Assets/BallerinaController.cs
+++ Assets/BallerinaController.cs
@@ -18,9 +18,28 @@ public class BallerinaController : MonoBehaviour
     }
 
 	void GetInputs() {
+		GetLegInputs();
+		GetButtonInputs();
+	}
+
+	void GetLegInputs() {
+		float rightStickX = Input.GetAxis("RightStickX");
+		animator.SetFloat("RightStickX", rightStickX);
 		float rightStickY = Input.GetAxis("RightStickY");
 		animator.SetFloat("RightStickY", rightStickY);
+		float leftStickX = Input.GetAxis("LeftStickX");
+		animator.SetFloat("LeftStickX", leftStickX);
 		float leftStickY = Input.GetAxis("LeftStickY");
 		animator.SetFloat("LeftStickY", leftStickY);
+	}
+
+	void GetButtonInputs() {
+		if(Input.GetButtonDown("SwitchPosition")) {
+			if(animator.GetInteger("Position") == 1) {
+				animator.SetInteger("Position", 5);
+			} else {
+				animator.SetInteger("Position", 1);
+			}
+		}
 	}
 }
