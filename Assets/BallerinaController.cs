bc12e7a4ba98a5e84ea9e3c07617e40e8fe558c2
gamesketches <sam.von.ehren@gmail.com>
2021-07-06 15:06:40 -0400

    First commit, blend file with all animations and such, basic tendu releve stuff

---
+++ Assets/BallerinaController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallerinaController : MonoBehaviour
+{
+	Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		GetInputs();
+    }
+
+	void GetInputs() {
+		float rightStickY = Input.GetAxis("RightStickY");
+		animator.SetFloat("RightStickY", rightStickY);
+		float leftStickY = Input.GetAxis("LeftStickY");
+		animator.SetFloat("LeftStickY", leftStickY);
+	}
+}
---
+++ Assets/DanceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceTest : MonoBehaviour
+{
+	Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log(Mathf.Sin((Time.realtimeSinceStartup + 1) / 2));
+        animator.SetFloat("Blend", Mathf.Sin((Time.realtimeSinceStartup + 1) / 2));
+    }
+}
