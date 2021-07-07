d55f8ca2842226998d4249a7b409ebf48343f470
gamesketches <sam.von.ehren@gmail.com>
2021-07-07 14:46:48 -0400

    Adding in spacebar to start music

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,8 +15,8 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if(Input.GetKeyDown(KeyCode.Space)) {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame) {
 			audioSource.Play();
-		}*/
+		}
     }
 }
