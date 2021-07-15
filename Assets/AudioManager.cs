ab4e6bda79948d163645cd1c22a76f520be5b3af
gamesketches <sam.von.ehren@gmail.com>
2021-07-14 21:33:36 -0400

    fixing sinking model, putting in cterrible homerolled beat detection, starting on card display and architecture

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -6,17 +6,35 @@ using UnityEngine.InputSystem;
 public class AudioManager : MonoBehaviour
 {
 	AudioSource audioSource;
+	public float songBPM;
+	public float secPerBeat;
+	public float songPosition;
+	public float songPositionInBeats;
+	public float dspSongTime;
+	
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+		secPerBeat = 60f / songBPM;
+		
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Keyboard.current.spaceKey.wasPressedThisFrame) {
+			dspSongTime = (float) AudioSettings.dspTime;
 			audioSource.Play();
+		}
+		if(audioSource.isPlaying) {
+			songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+			songPositionInBeats = songPosition / secPerBeat;
+			if(Keyboard.current.aKey.wasPressedThisFrame) {
+				if(songPositionInBeats % 1 > 0.8f || songPositionInBeats % 1 < 0.2f) {
+					Debug.Log("Nice!");
+				} else Debug.Log(songPositionInBeats %1);
+			}
 		}
     }
 }
---
+++ Assets/ChoreographerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum MoveType {TenduFront, TenduSide, TenduBack, Releve, Plie, SwitchPosition, CloseBack};
+
+public class ChoreographerController : MonoBehaviour
+{
+	public Move[] choreography;
+	public GameObject moveCardPrefab;
+	public Transform canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       for(int i = 0; i < choreography.Length; i++) {
+			GameObject newMoveCard = Instantiate(moveCardPrefab, canvas);
+			newMoveCard.GetComponent<RectTransform>().anchoredPosition = new Vector2(-100f, -63.1f + (i * -100f));
+			newMoveCard.GetComponent<Image>().sprite = Resources.Load<Sprite>(choreography[i].moveType.ToString());
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class Move {
+	public MoveType moveType;
+	public float startBeat;
+	public float trueStartBeat;
+	public float targetBeat;
+	
+	public Move(MoveType theMove, float startingBeat, float trueStartingBeat, float theTargetBeat) {
+		moveType = theMove;
+		startBeat = startingBeat;
+		trueStartBeat = trueStartingBeat;
+		targetBeat = theTargetBeat;
+	}
+}
